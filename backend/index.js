b2359f3cb608ee72f9d26f026bb9c02c22ae5f9a
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-09-11 11:25:59 -0300

    11/09

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const PORT = 3000;
 
 // app.listen(3000, ()=> console.log("Deu baum!"));
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // app.get('/', (req, res)=> {
 //     res.send("Bem-vindo(a) à nossa primeira aplicação. ola")
@@ -29,12 +30,26 @@ const conection = mysql.createPool({
     password: ''
 });
 
-const getAllPessoas = async() => {
+const getAllPessoas = async () => {
     const [query] = await conection.execute('select * from pessoa')
     return query
 };
 
-app.get('/pessoa', async(req, res) => {
+app.get('/pessoa', async (req, res) => {
     const resultado = await getAllPessoas()
     return res.status(200).json(resultado)
+});
+
+app.get('/pessoa/:id', async (req, res) => {
+    const { id } = req.params;
+    const [query] = await conection.execute('select * from pessoa where id = ?', [id]);
+    if (query.length === 0) return res.status(400).json({ mensagem: 'nenhuma pessoa encontrada' });
+    return res.status(200).json(query);
+});
+
+app.post('/pessoa', async (req, res) => {
+    const { nome, email } = req.body;
+    const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)', [nome, email]);
+
+    return res.json(query);
 });
